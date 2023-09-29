3ff5e142d40298da1e09a6d061f7be7626d40020
owencali <oweeengk@gmail.com>
2023-09-29 13:13:08 +0800

    Create html boilerplates for different URLs and properly pathing them

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "homepage"
 urlpatterns = [
   path("", views.index, name="index"),
+  path("about/", views.about, name="about"),
+  path("news/", views.news, name="news"),
+  path("volunteering/", views.volunteering, name="volunteering"),
+  path("projects/", views.projects, name="projects"),
+  path("tesda/", views.tesda, name="tesda"),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -6,3 +6,18 @@ from django.http import Http404
 def index(request):
   #return HttpResponse("WTF")
   return render(request, "homepage.html")
+
+def about(request):
+  return render(request, "about.html")
+
+def news(request):
+  return render(request, "news.html")
+
+def volunteering(request):
+  return render(request, "volunteering.html")
+
+def projects(request):
+  return render(request, "projects.html")
+
+def tesda(request):
+  return render(request, "tesda.html")
