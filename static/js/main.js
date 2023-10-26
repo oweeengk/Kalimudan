517ea93cfb4916135475d1a65f615daeb67d1174
oweeengk <oweeengk@gmail.com>
2023-10-27 03:29:42 +0800

    Working navbar with minor issues on dropdown-items

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,43 @@
+// Function to add the 'active' class to the appropriate link based on the current page
+function setActiveLink() {
+  const currentURL = window.location.href
+  const dropdownLinks = document.querySelectorAll(".nav-item.dropdown")
+
+  dropdownLinks.forEach(function (element) {
+    const dropdownLink = element.querySelector(".nav-link.dropdown-toggle")
+    const linkURL = dropdownLink.getAttribute("href")
+
+    if (currentURL === linkURL) {
+      dropdownLink.classList.add("active")
+    } else {
+      dropdownLink.classList.remove("active")
+    }
+  })
+}
+
+// Add 'active' class to the appropriate link on page load
+setActiveLink()
+
+// Mouseover event to show the dropdown menu on hover
 document.querySelectorAll(".nav-item.dropdown").forEach(function (element) {
-  // Mouseover event to show the dropdown menu
+  const dropdownLink = element.querySelector(".nav-link.dropdown-toggle")
+  const dropdownMenu = element.querySelector(".dropdown-menu")
+
   element.addEventListener("mouseover", function () {
-    element.querySelector(".dropdown-menu").classList.add("show")
-    element.querySelector(".nav-link.dropdown-toggle").classList.add("active")
+    dropdownLink.classList.add("active")
+    dropdownMenu.classList.add("show")
   })
 
-  // Mouseout event to hide the dropdown menu
+  // Mouseout event to hide the dropdown menu on mouse away
   element.addEventListener("mouseout", function () {
-    if (
-      !element
-        .querySelector(".nav-link.dropdown-toggle")
-        .classList.contains("pressed")
-    ) {
-      element.querySelector(".dropdown-menu").classList.remove("show")
-      element
-        .querySelector(".nav-link.dropdown-toggle")
-        .classList.remove("active")
-    }
+    dropdownLink.classList.remove("active")
+    dropdownMenu.classList.remove("show")
   })
 
   // Handle click event on the dropdown link
-  element
-    .querySelector(".nav-link.dropdown-toggle")
-    .addEventListener("click", function (e) {
-      e.preventDefault() // Prevent the link from navigating immediately
-      window.location.href = this.getAttribute("href") // Navigate to the specified URL
-      this.classList.add("pressed")
-    })
+  dropdownLink.addEventListener("click", function (e) {
+    e.preventDefault()
+    window.location.href = this.getAttribute("href")
+    this.classList.add("pressed")
+  })
 })
