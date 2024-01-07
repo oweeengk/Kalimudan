c797b65db5ab4ac26266a67fcae330795289feed
oweeengk <oweeengk@gmail.com>
2024-01-08 07:23:20 +0800

    Remove unnecessary URLs and Views

--- homepage/urls.py
+++ homepage/urls.py
@@ -14,10 +14,8 @@ urlpatterns = [
   path("about/visitkfi", views.visitkfi, name="visitkfi"),
   path("about/accreditations", views.accreditations, name="accreditations"),
   path("about/administration", views.administration, name="administration"),
-  path("about/factsandfigures", views.factsandfigures, name="factsandfigures"),
   path("about/manualofoperations", views.manualofoperations, name="manualofoperations"),
 
-  
   path('stories/create/', views.StoriesCreateView.as_view(), name='stories_create'),
   path('stories/<str:category>/', views.StoriesListView.as_view(), name='stories_list'),
   path('stories/<str:category>/<slug:slug>/', views.stories_detail, name='stories_detail'),
@@ -26,13 +24,12 @@ urlpatterns = [
   path('stories/', views.StoriesListView.as_view(), name='stories_list'),
 
   path("volunteering/apply", views.volunteeringapply, name="volunteeringapply"),
-  path("volunteering/notices", views.volunteeringnotices, name="volunteeringnotices"),
 
   path("projects/information", views.projectsinformation, name="projectsinformation"),
   path("projects/partners", views.projectspartners, name="projectspartners"),
 
   path("tesda/coursesoffered", views.tesdacoursesoffered, name="tesdacoursesoffered"),
-  path("tesda/information", views.tesdainformation, name="tesdainformation"),
+  path("tesda/history", views.tesdahistory, name="tesdahistory"),
 
   path('db/upload', views.file_upload, name='file_upload'),
   path('db/files', views.file_list, name='file_list'),
